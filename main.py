072ee2f66ecfbfb988934520f4538b1d018bb301
seollove <seollove@192.168.nate.com>
2023-02-16 22:41:14 +0900

    rtsp_url qeurystring으로 받으서 처리 할수 있도록 수정

--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 # StreamingResponse를 가져와야함
 from fastapi import FastAPI
 from fastapi.responses import StreamingResponse
+from typing import Union
 
 # cv2 모듈 import
 from cv_module import get_stream_video
@@ -12,15 +13,15 @@ from cv_module import get_stream_video
 app = FastAPI()
 
 # openCV에서 이미지 불러오는 함수
-def video_streaming():
-    return get_stream_video("rtsp://admin:suhwooms0792%@61.82.34.191:554/Streaming/Channels/0401")
+def video_streaming(rtsp_url):
+    return get_stream_video(rtsp_url)
 
 # 스트리밍 경로를 /video 경로로 설정.
 @app.get("/video")
-def main():
+def main(rtsp_url: Union[str, int] = 0):
     # StringResponse함수를 return하고,
     # 인자로 OpenCV에서 가져온 "바이트"이미지와 type을 명시
-    return StreamingResponse(video_streaming(), media_type="multipart/x-mixed-replace; boundary=frame")
+    return StreamingResponse(video_streaming(rtsp_url), media_type="multipart/x-mixed-replace; boundary=frame")
 
 @app.get("/test")
 def test():
